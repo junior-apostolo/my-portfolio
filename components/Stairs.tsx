dd8aa8bc39496480bd1d440ae7db7c3a323a4c3f
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2024-05-25 12:42:41 -0300

    feat: finalizando animação de transição e NavHeader

--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -12,8 +12,33 @@ const stairAnimation = {
   },
 };
 
+const reverseIndex = (index: number) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
+
 const Stairs = () => {
-  return <div>Stairs</div>;
+  return (
+    <>
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          ></motion.div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Stairs;
