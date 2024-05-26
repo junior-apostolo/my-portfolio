4523c26a009a92897795f2a5ce97be35823de563
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2024-05-25 23:55:57 -0300

    feat: creating mobileNav, refactoring shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import {Button} from '@/components/ui/button';
+import {FiDownload} from 'react-icons/fi'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world!!
-    </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8">
+          <div>text</div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
 }
---
+++ components/MobileNav.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  { name: "home", path: "/" },
+  { name: "services", path: "/services" },
+  { name: "projects", path: "/projects" },
+  { name: "resume", path: "/resume" },
+  { name: "contact", path: "/contact" },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-stress" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Sergio<span className="text-stress">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${link.path === pathname && "text-stress border-b-2 border-stress"} text-xl capitalize hover:text-stress transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
